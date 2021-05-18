bf485cb0147d95c69242ae4532607292fd940108
Lauren Koenig <lrnkoenig@gmail.com>
2021-05-18 13:48:10 -0400

    Add edits to R script summarizing width data

--- Estimate_width.R
+++ Estimate_width.R
@@ -1,11 +1,14 @@
 ## StreamPULSE synthesis: Estimate site width
-## Last updated 11 May 2021
+## Last updated 18 May 2021
 ## LE Koenig
 
-library(tidyr)    # data manipulation
-library(dplyr)    # data manipulation
-library(ggplot2)  # create plots
-library(sf)       # work with geospatial data
+library(tidyr)        # data manipulation
+library(dplyr)        # data manipulation
+library(ggplot2)      # create plots
+library(patchwork)    # format plots
+library(waffle)       # create plots to visualize data sources
+library(sf)           # work with geospatial data
+library(nhdplusTools) # interface with national hydrography dataset
 
 ####################################################
 ##                    Load data                   ##
@@ -54,12 +57,12 @@ reg_coef <- read.csv("./data/RegionalHydraulicCoef.csv",header=TRUE,stringsAsFac
     widths_manual_GE <- read.csv("./data/manual_subset/sites_manual_width.csv",header=TRUE)
     widths_GE <- widths_manual_GE %>% select(!c(lat,lon,comments)) %>% 
                   pivot_longer(!sitecode, names_to = "width_xsection", values_to = "value") %>% 
-                  group_by(sitecode) %>% summarize(width_m_GE = mean(value,na.rm=T))
+                  group_by(sitecode) %>% summarize(width_m_GE = median(value,na.rm=T))
     
     # ArcMap:
     widths_manual_arc <- read.csv("./data/manual_subset/sites_manual_width_arc.csv",header=TRUE)
     widths_arc <- widths_manual_arc %>% select(!OID) %>% rename("width_xsection"="xsection","value"="width_m") %>%
-              group_by(sitecode) %>% summarize(width_m_arc = mean(value,na.rm=T))
+              group_by(sitecode) %>% summarize(width_m_arc = median(value,na.rm=T))
     
     # StreamPULSE data portal:
     files <- list.files("./data/SP_portal_site_data/",pattern="geomorph")
@@ -67,24 +70,39 @@ reg_coef <- read.csv("./data/RegionalHydraulicCoef.csv",header=TRUE,stringsAsFac
     SP_dat_ls <- list()
     
     for(i in 1:length(files)){
+      site <- substr(files[i], start = 1, stop = 2)
       dat <- read.csv(paste("./data/SP_portal_site_data/",files[i],sep=""),header=TRUE,skip = 1) %>% rename("sitecode"="siteID")
-      dat_summary <- dat[,c("sitecode","date","wetted_width_m")] %>% group_by(sitecode) %>% 
-                     summarize(width_m_SP = mean(wetted_width_m,na.rm=T)) 
+      dat_summary <- dat[,c("sitecode","date","wetted_width_m")] %>% group_by(sitecode,date) %>% 
+        summarize(width_m = median(wetted_width_m,na.rm=T)) %>% group_by(sitecode) %>%
+        summarize(width_m_SP = median(width_m,na.rm=T))
+      for(j in 1:length(dat_summary$sitecode)){
+        dat_summary$sitecode[j] <- ifelse(grepl(site, dat_summary$sitecode[j], fixed = TRUE),
+                                          dat_summary$sitecode[j],
+                                          paste(site,"_",dat_summary$sitecode[j],sep=""))
+      }
+      
       SP_dat_ls[[i]] <- dat_summary
     }
     
     SP_dat <- do.call(rbind,SP_dat_ls)
     SP_dat$width_m_SP[which(is.nan(SP_dat$width_m_SP))] <- NA
     
+    # NH EPSCoR site not on the data portal (NH_DCF):  
+    epscor_dat <- read.csv("./data/NH_EPSCoR_site_data/01_EPSCoR_Stream_Dims.csv",header=TRUE) %>% filter(Site=="DCF") %>%
+      group_by(Date) %>% summarize(MedWidth_m = median(Width_cm,na.rm=TRUE)) %>% summarize(width_m_SP = median(MedWidth_m,na.rm=TRUE)/100) %>% mutate(sitecode = "NH_DCF") %>% select(sitecode,width_m_SP)
+    
+    # Combine SP_dat with new EPSCoR data above:
+    SP_dat2 <- bind_rows(SP_dat,epscor_dat)
+    
     # Combine manual width data:
-    widths_manual <- left_join(widths_GE,widths_arc,by="sitecode") %>% bind_rows(.,SP_dat)
-
-# Compare manual width estimates:
+    widths_manual <- left_join(widths_GE,widths_arc,by="sitecode") %>% left_join(.,SP_dat2) %>% bind_rows(.,SP_dat2[! SP_dat2$sitecode %in% .$sitecode,])
+    
+# Compare manual width estimates (since GE estimates of distance may not be very accurate):
 plot(width_m_GE~width_m_arc,data = widths_manual)
 abline(a=0,b=1)
 
 sites <- left_join(sites,widths_manual,by="sitecode")
-
+head(sites)
 
 ####################################################
 ##             Calculate NWIS widths              ##
@@ -160,6 +178,9 @@ sites$width_raymond <- apply(sites["sitecode"],1,calc_width_raymond)
 # Coefficients estimated based on EPA NRSA and WSA data, see: https://github.com/lekoenig/US-hydraulic-geometry.git
 sites$width_natlEPA <- 10^((log10(sites$ws_area_km2) * 0.455)-0.06)
 
+# For site in Florida with 0 upstream area, set width to NA:
+sites$width_natlEPA[which(sites$ws_area_km2==0)] <- NA
+
 ####################################################
 ##             Widths: EPA Regional               ##
 ####################################################
@@ -169,6 +190,8 @@ sites <- left_join(sites,reg_coef[,c("HUC02","coef_slope","coef_int")],by=c("vpu
 # Coefficients estimated based on EPA NRSA and WSA data, see: https://github.com/lekoenig/US-hydraulic-geometry.git
 sites$width_reglEPA <- 10^((log10(sites$ws_area_km2) * sites$coef_slope)+sites$coef_int)
 
+# For site in Florida with 0 upstream area, set width to NA:
+sites$width_reglEPA[which(sites$ws_area_km2==0)] <- NA
 
 ####################################################
 ##   Randomly select sites for manual validation  ##
@@ -182,31 +205,32 @@ sites$width_reglEPA <- 10^((log10(sites$ws_area_km2) * sites$coef_slope)+sites$c
 ##      Compare widths against validation data    ##
 ####################################################
 
-sites$width_manual <- ifelse(!is.na(sites$width_m_SP),sites$width_m_SP,sites$width_m_arc)
+# define the manual data we're using:
+sites$width_manual <- ifelse(!is.na(sites$width_m_SP),sites$width_m_SP,sites$width_m_GE)
 
-# add colors for sites with dam flag > 50
+# add colors for sites with dam flag > 50:
 sites$dam_color <- ifelse(!is.na(sites$struct.dam_flag) & sites$struct.dam_flag>=50,"flag","ok")
 
+# plot estimated widths against manual ~validation data:
 plot.mcman <- sites %>% ggplot() + geom_point(aes(x=width_manual,y=width_mcmanamay,color=dam_color),alpha=.75) + geom_abline(slope=1,intercept=0,lty=2)+
-              theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
-              ggtitle("McManamay") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none")
-plot.nwis <- sites %>% ggplot() + geom_point(aes(x=width_manual,y=width_nwis,color=dam_color),alpha=.75) + geom_abline(slope=1,intercept=0,lty=2) +
-             theme_classic()+ coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
-             ggtitle("NWIS coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none")
+      theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
+      ggtitle("McManamay") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none",title = element_text(size=10))
+plot.nwis <- sites %>% ggplot() + geom_point(aes(x=width_manual,y=width_nwis,color=dam_color),alpha=.75) + geom_abline(slope=1,intercept=0,lty=2) + 
+      theme_classic()+ coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
+      ggtitle("NWIS coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none",title = element_text(size=10))
 plot.raymond <- sites %>% ggplot() + geom_point(aes(x=width_manual,y=width_raymond,color=dam_color),alpha=.75) + geom_abline(slope=1,intercept=0,lty=2) +
-             theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
-             ggtitle("Raymond coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none")
+      theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
+      ggtitle("Raymond coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none",title = element_text(size=10))
 plot.natlEPA <- sites %>% ggplot() + geom_point(aes(x=width_manual,y=width_natlEPA,color=dam_color),alpha=.75) + geom_abline(slope=1,intercept=0,lty=2)+
-             theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
-             ggtitle("Natl EPA coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none") 
+      theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
+      ggtitle("Natl EPA coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none",title = element_text(size=10)) 
 plot.reglEPA <- sites %>% ggplot() + geom_point(aes(x=width_manual,y=width_reglEPA,color=dam_color),alpha=.75) + geom_abline(slope=1,intercept=0,lty=2) +
-             theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
-             ggtitle("Regional EPA coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none")
-plots <- plot.mcman + plot.nwis + plot.raymond + plot.natlEPA + plot.reglEPA + plot_layout(ncol=2)
+      theme_classic() + coord_cartesian(xlim=c(0,300),ylim=c(0,300)) + scale_color_manual(values=c("#009F73","black"))+
+      ggtitle("Regional EPA coefficients") + labs(x=expression(width~manual~(m)),y=expression(width~predicted~(m))) + theme(legend.position="none",title = element_text(size=10))
+plots <- plot.mcman + plot.nwis + plot.raymond + plot.natlEPA + plot.reglEPA + plot_layout(ncol=3)
 print(plots)
 
 # Compare linear regression and RMSE:
-
 fits.df <- data.frame(Width_est_method = c("McManamay","NWIS coefficients","Raymond coefficients","Natl EPA coefficients","Regional EPA coefficients"),
                       slope = NA,
                       r2 = NA,
@@ -223,11 +247,18 @@ for(i in 1:length(fits.df$Width_est_method)){
   fits.df$slope[i] <- round(as.numeric(fit$coefficients[2]),2)
   fits.df$r2[i] <- round(as.numeric(summary(fit)$r.squared),2)
   fits.df$RMSE[i] <- round(sqrt(mean(fit$residuals^2)),1)
-  
 }
+print(fits.df)
 
+# If we estimate width using NWIS at-a-site relationships, how many sites are we missing widths for?
+n.SP <- length(which(SP_dat2$sitecode %in% sites$sitecode))
+sites$source <- ifelse(sites$sitecode %in% SP_dat2$sitecode,"StreamPULSE estimates",ifelse(sites$sitecode %in% appling_info$site_name,"NWIS estimates","No estimate"))
+counts <- sites %>% group_by(source) %>% summarise(n = n()) %>%  mutate(percent = round(n/sum(n)*100))
+case_counts <- counts$n
+names(case_counts) <- counts$source
+print(case_counts)
 
-
+waffle::waffle(case_counts,rows=10)
 
 
 
